478b733642fcb9d44290430e508124aa4d0cc340
XRTIDE <xrtide@gmail.com>
2024-03-05 17:29:51 +0000

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use std::process::Command;
-
 fn main() {
-    let output = Command::new("ffmpeg")
+    let input_path = Path::new("weishu.mp4");
+    let output_path = Path::new("weishu.mp3");
+    audio_extract(input_path, output_path).unwrap();
+}
+
+fn audio_extract(input_path: &Path, output_path: &Path) -> std::io::Result<()> {
+    let mut file = File::open(input_path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    Command::new("ffmpeg")
         .arg("-i")
-        .arg("input.mp3")
-        .arg("output.wav")
+        .arg("pipe:0")
+        .arg("-vn")
+        .arg(output_path.to_str().unwrap())
         .output()
         .expect("Failed to execute command");
 
-    if !output.status.success() {
-        eprintln!("未成功: {:?}", output.stderr);
-    }
+    Ok(())
 }
